8a3fc2e0be443d9dcb72544fdf68408eb7be1ced
Alex <147496294+A-Hoenig@users.noreply.github.com>
2024-04-17 19:19:13 +0200

    refactor folder structure and add questions

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,8 @@ import Question from './Question';
 const initialState = {
   questions: [],
   // loading, error, ready, active, finished
-  status: 'loading'
+  status: 'loading',
+  index: 0,
 }
 function reducer (state, action) {
   switch(action.type) {
@@ -32,7 +33,7 @@ function reducer (state, action) {
 }
 
 export default function App (){
-  const [{questions, status}, dispatch] = useReducer(reducer, initialState)
+  const [{questions, status, index}, dispatch] = useReducer(reducer, initialState)
 
   const numQuestions = questions.length;
 
@@ -50,7 +51,7 @@ export default function App (){
       {status === 'loading' && <Loader />}
       {status === 'error' && <Error />}
       {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-      {status === 'active' && <Question />}
+      {status === 'active' && <Question question={questions[index]}/>}
     </Main>
   </div>
 }
